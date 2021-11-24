b7832aea26eff5702437d9bb4353bc3f26ec7a79
xtimule <gxumoyun1@gmail.com>
2021-11-24 20:00:08 +0500

    a

--- goods/settings.py
+++ goods/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lke$(x*fouopfe68inr7wab%2*9c9t_ibg*@81&nrnajifi$u#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['domen','81.95.232.205']
 
 
 # Application definition
