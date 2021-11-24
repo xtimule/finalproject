30afd29da716e2ec315b49bbc544a32a3ec40bb1
xtimule <gxumoyun1@gmail.com>
2021-11-24 20:08:16 +0500

    fix: comma

--- goods/settings.py
+++ goods/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lke$(x*fouopfe68inr7wab%2*9c9t_ibg*@81&nrnajifi$u#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['project.uz','81.95.232.205']
+ALLOWED_HOSTS = ['project.uz','81.95.232.205','127.0.0.1']
 
 
 # Application definition
