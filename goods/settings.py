f9cce33d62365718a0b07da1137bd72cad9b35aa
xtimule <gxumoyun1@gmail.com>
2021-12-01 23:10:56 +0500

    db updated

--- goods/settings.py
+++ goods/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'goodsdb',
         'USER': 'postgres',
-        'PASSWORD': 'qwertyc1',
+        'PASSWORD': 'goodsdb',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
