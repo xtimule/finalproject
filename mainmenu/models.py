f2e31d0bbebfb98fb6db207eef3faff3ee9003d4
xtimule <gxumoyun1@gmail.com>
2021-12-01 23:26:40 +0500

    db updated

--- mainmenu/models.py
+++ mainmenu/models.py
@@ -15,6 +15,6 @@ class Tovar(models.Model):
     category = models.ForeignKey(Category, related_name='tovary', on_delete=models.CASCADE)
     brand = models.CharField(max_length=50, default='')
     weight = models.IntegerField()
-    shtrix = models.IntegerField()
+    shtrix = models.CharField(max_length=50)
     manufacturer = models.CharField(max_length=50)
     number = models.IntegerField(default=0)
