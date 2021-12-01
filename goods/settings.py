48c9787e34f3874684fe2fb4c549e36c72e4aa11
xtimule <gxumoyun1@gmail.com>
2021-12-01 20:56:49 +0500

    db updated

--- goods/settings.py
+++ goods/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'goods.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'goodsdb',
+        'USER': 'postgres',
+        'PASSWORD': 'qwertyc1',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
