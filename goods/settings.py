7d3cba4e9738b70f5bc622123a371ca8232b827f
xtimule <gxumoyun1@gmail.com>
2021-12-03 21:49:42 +0500

    static added

--- goods/settings.py
+++ goods/settings.py
@@ -139,5 +139,9 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static-files')
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+    'goods/static'
+]
 MEDIA_URL = 'media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
