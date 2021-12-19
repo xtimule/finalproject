5399f6393fc1b2949777e32c713ed07bce1d6fb6
xtimule <gxumoyun1@gmail.com>
2021-12-19 13:10:41 +0500

    order added

--- mainmenu/models.py
+++ mainmenu/models.py
@@ -23,10 +23,15 @@ class Tovar(models.Model):
         return self.name
 class Card(models.Model):
     user = models.ForeignKey(get_user_model(), related_name="cards", on_delete=models.PROTECT, default=None)
-
+    is_sold = models.BooleanField(default=False)
     added_date = models.DateTimeField(auto_now_add=True)
-    session_id = models.CharField(max_length=200, default='')
-
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=25)
+    email = models.EmailField()
+    organization = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    address = models.CharField(max_length=250)
+    comments = models.TextField()
     def __str__(self) -> str:
         return f"{self.user.username}: {self.added_date}"
 
@@ -35,7 +40,6 @@ class CardItem(models.Model):
     product = models.ForeignKey(Tovar, related_name="carditems", on_delete=models.PROTECT, default=None)
     total = models.IntegerField(default=1)
     card = models.ForeignKey(Card, related_name="carditems", on_delete=models.CASCADE, default=None)
-    is_active = models.BooleanField(default=True)
 
     def __str__(self) -> str:
         return self.product.name
